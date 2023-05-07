ee172c5520d43e5b51592101127fbf15a380d4b8
Silleellie <antonio.silletti@gmail.com>
2023-05-07 20:24:08 +0200

    now also possible values of attributes are extracted

--- src_java/xmlschemaelement/XMLSchemaElement.java
+++ src_java/xmlschemaelement/XMLSchemaElement.java
@@ -1,5 +1,6 @@
 package src_java.xmlschemaelement;
 
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -11,7 +12,7 @@ import java.util.HashMap;
 abstract class XMLSchemaElement {
 
     public String name;
-    public ArrayList<HashMap<String, String>> attributes;
+    public ArrayList<HashMap<String, Object>> attributes;
     protected HashMap<String, NodeList> itemContentTree;
 
     public XMLSchemaElement(Node itemNode) {
@@ -39,7 +40,7 @@ abstract class XMLSchemaElement {
 
     public ArrayList<HashMap<String, String>> extractAttributes(NodeList attributesNodeList) {
 
-        ArrayList<HashMap<String, String>> attributes = new ArrayList<>();
+        ArrayList<HashMap<String, Object>> attributes = new ArrayList<>();
 
         for (int i = 0; i < attributesNodeList.getLength(); i++) {
             Node node = attributesNodeList.item(i);
@@ -47,7 +48,7 @@ abstract class XMLSchemaElement {
 
                 NamedNodeMap nodeAttributes = node.getAttributes();
 
-                HashMap<String, String> attribute = new HashMap<>();
+                HashMap<String, Object> attribute = new HashMap<>();
 
                 for (int j = 0; j < nodeAttributes.getLength(); j++) {
 
@@ -57,6 +58,12 @@ abstract class XMLSchemaElement {
                     attribute.put(key, value);
                 }
 
+                if (attribute.get("datatype").equals("select")) {
+
+                    ArrayList<String> possibleValues = extractPossibleValues(node);
+                    attribute.put("values", possibleValues);
+                }
+
                 attributes.add(attribute);
             }
         }
@@ -64,4 +71,23 @@ abstract class XMLSchemaElement {
         return attributes;
     }
 
+    private ArrayList<String> extractPossibleValues(Node attributeNode) {
+
+        ArrayList<String> possibleValues = new ArrayList<>();
+
+        // we can't have hierarchy of attributes, so this is enough w/o cycling
+        // all child nodes
+        NodeList possibleValuesNodes = ((Element) attributeNode).getElementsByTagName("value");
+
+        for (int i = 0; i < possibleValuesNodes.getLength(); i++) {
+
+            Node singleValue = possibleValuesNodes.item(i);
+            String val = singleValue.getAttributes().getNamedItem("name").getNodeValue();
+
+            possibleValues.add(val);
+
+        }
+
+        return possibleValues;
+    }
 }
