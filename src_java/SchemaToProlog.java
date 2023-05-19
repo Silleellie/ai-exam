19289abf50cf044bd80d352ec0808e1ad18a5249
Silleellie <antonio.silletti@gmail.com>
2023-05-19 14:33:37 +0200

    added high level info in schema translated (directives, hierarchy, etc.)
    Co-authored-by: m-elio <m-elio@users.noreply.github.com>

--- src_java/SchemaToProlog.java
+++ src_java/SchemaToProlog.java
@@ -22,16 +22,37 @@ public class SchemaToProlog {
         String inputFilename = input.nextLine();
 
         // if input filename has extension, strip it and then append .pl
-        String outputFilename = (inputFilename.contains(".")) ?
+        String outputFilenameKb = (inputFilename.contains(".")) ?
                 inputFilename.substring(0, inputFilename.lastIndexOf('.')) :
                 inputFilename;
-        outputFilename = "%s.pl".formatted(outputFilename);
-
+        outputFilenameKb = "%s.pl".formatted(outputFilenameKb);
+        
         XMLSchemaScanner xml = new XMLSchemaScanner(inputFilename);
+        FileWriter kbWriter = new FileWriter(outputFilenameKb);
 
-        FileWriter kbWriter = new FileWriter(outputFilename);
+        writeDirectives(kbWriter);
+        writeEntities(xml, kbWriter);
+        writeRelationships(xml, kbWriter);
+        writeRules(kbWriter);
 
-        kbWriter.write("% ENTITIES\n\n");
+        kbWriter.close();
+
+        String outputPath = FileSystems.getDefault().getPath(outputFilenameKb).toAbsolutePath().toString();
+        System.out.printf("Converted schema saved into %s!%n", outputPath);
+
+    }
+
+    public static void writeDirectives(FileWriter kbWriter) throws IOException {
+
+        kbWriter.write(":- use_module(library(lists)).\n");
+        kbWriter.write(":- discontiguous inverse_of/2.\n\n\n");
+
+    }
+
+    public static void writeEntities(XMLSchemaScanner xml, FileWriter kbWriter) throws IOException {
+
+        StringBuilder subclassOfFacts = new StringBuilder();
+        StringBuilder entitiesFacts = new StringBuilder();
 
         Iterator<Entity> itEntities = xml.iteratorEntities();
         while (itEntities.hasNext()) {
@@ -39,97 +60,201 @@ public class SchemaToProlog {
             Entity originalClass = itEntities.next();
 
             // prologFact will contain facts for all the various subclasses separated by \n
-            String prologFact = String.join("\n", obtainPrologFact(originalClass));
-            kbWriter.write(prologFact);
+
+            HashMap<String, ArrayList<String>> facts = obtainPrologFact(originalClass);
+
+            subclassOfFacts.append(String.join("\n", facts.get("subclassOfFacts")));
+            entitiesFacts.append(String.join("\n", facts.get("entityFacts")));
 
             // double visual separator for different taxonomies
-            kbWriter.write("\n");
-            kbWriter.write("\n");
-        }
-
-        kbWriter.write("\n\n% RELATIONSHIPS\n\n");
-
-        Iterator<Relationship> itRelationships = xml.iteratorRelationships();
-        while (itRelationships.hasNext()) {
-
-            Relationship originalRelationship = itRelationships.next();
-            String prologFact = obtainPrologFact(originalRelationship);
-            kbWriter.write(prologFact + "\n");
-
-            // write inverse relationship
-            if (originalRelationship.inverseRelationship != null) {
-                kbWriter.write(obtainPrologFact(originalRelationship.inverseRelationship) + "\n");
+            if (facts.get("subclassOfFacts").size() != 0) {
+                subclassOfFacts.append("\n\n");
             }
 
+            entitiesFacts.append("\n\n");
         }
 
-        kbWriter.close();
+        kbWriter.write("% ENTITIES\n\n");
 
-        String outputPath = FileSystems.getDefault().getPath(outputFilename).toAbsolutePath().toString();
-        System.out.printf("Converted schema saved into %s!%n", outputPath);
+        kbWriter.write("% Entities hierarchy\n\n");
+        kbWriter.write(subclassOfFacts.toString());
+
+        kbWriter.write("% Entities schema\n\n");
+        kbWriter.write(entitiesFacts.toString() + "\n");
+
     }
 
-    public static ArrayList<String> obtainPrologFact(Entity xmlEntity) {
+    public static void writeRelationships(XMLSchemaScanner xml, FileWriter kbWriter) throws IOException {
 
-        ArrayList<String> factList = new ArrayList<>();
+        Iterator<Relationship> itRelationships = xml.iteratorRelationships();
 
-        // wrap name with quotes to respect format of class name (mantain "/", first upper letter, ...)
-        String predicateName = "'%s'".formatted(xmlEntity.name);
+        kbWriter.write("% RELATIONSHIPS\n\n");
 
-        // extracting attributes name
-        ArrayList<String> attributeList = new ArrayList<>();
+        while (itRelationships.hasNext()) {
 
-        for (HashMap<String, Object> attribute : xmlEntity.attributes) {
-            attributeList.add((String) attribute.get("name"));
+            Relationship originalClass = itRelationships.next();
 
+            // prologFact will contain facts for all the various subclasses separated by \n
+
+            HashMap<String, ArrayList<String>> facts = obtainPrologFact(originalClass);
+
+            kbWriter.write(String.join("\n", facts.get("relationshipFacts")));
+
+            if (facts.get("inverseOfFacts").size() != 0) {
+                kbWriter.write("\n");
+                kbWriter.write(String.join("\n", facts.get("inverseOfFacts")));
+            }
+
+            kbWriter.write("\n\n");
         }
 
-        String arguments = "[%s]".formatted(String.join(", ", attributeList));
-        String originalFact = "%s(%s).".formatted(predicateName, arguments);
+        kbWriter.write("\n");
 
-        factList.add(originalFact);
+    }
+
+    public static void writeRules(FileWriter kbWriter) throws IOException {
+
+        kbWriter.write("% GENERIC RULES\n\n");
+
+        // ################ Write is_subclass predicate ################
+        String isSubclass1 =
+                """
+                is_subclass(Subclass, Superclass) :-
+                \tsubclass_of(Subclass, Superclass),
+                \t\\=(Subclass, Superclass).""";
+
+        String isSubclass2 =
+                """
+                is_subclass(Subclass, Superclass) :-
+                \tsubclass_of(Subclass, Middleclass),
+                \tis_subclass(Middleclass, Superclass).
+                """;
+
+        kbWriter.write("%s\n%s\n\n".formatted(isSubclass1, isSubclass2));
+
+
+        // ################ write invert_subj_obj predicate ################
+        String baseInvertSubjObj = "invert_subj_obj([], []).";
+        String stepinvertSubjObj =
+                """
+                invert_subj_obj([Subject-Object|T1], [Object-Subject|T2]) :-
+                \tinvert_subj_obj(T1, T2).""";
+
+        String invertSubjObj = String.format("%s\n%s", baseInvertSubjObj, stepinvertSubjObj);
+
+        // ################ write invert_relationship predicate ################
+        String invertRelationship =
+                """
+                invert_relationship(RelationshipName, InvertedRelationship) :-
+                \tRelationshipToInvert =.. [RelationshipName, SubjObjList, AttributeList],
+                \tcall(RelationshipToInvert),
+                \tinvert_subj_obj(SubjObjList, InvertedSubjObjList),
+                \tinverse_of(InvertedRelationshipName, RelationshipName),
+                \tInvertedRelationship =.. [InvertedRelationshipName, InvertedSubjObjList, AttributeList].
+                """;
+
+        kbWriter.write(String.format("%s\n\n%s\n\n", invertSubjObj, invertRelationship));
+
+
+        // ################ write gather_parent_attributes predicate ################
+        String baseGatherParentAttributes = "gather_parent_attributes([], []).";
+        String stepGatherParentAttributes =
+                """
+                gather_parent_attributes([ParentC|ParentClasses], [ParentCAttributes|OtherParentAttributes]) :-
+                \tClause =.. [ParentC, ParentCAttributes],
+                \tcall(Clause),
+                \tgather_parent_attributes(ParentClasses, OtherParentAttributes).""";
+
+        kbWriter.write(String.format("%s\n%s\n\n", baseGatherParentAttributes, stepGatherParentAttributes));
+
+        // ################ write gather_attributes predicate ################
+        String gatherAttributes = """
+                gather_attributes(SubC, Attributes) :-
+                \tfindall(SuperC, is_subclass(SubC, SuperC), L),
+                \tgather_parent_attributes(L, ParentAttributes),
+                \tClause =.. [SubC, SubCAttributes],
+                \tcall(Clause),
+                \tflatten([ParentAttributes|SubCAttributes], Attributes).""";
+
+        kbWriter.write(gatherAttributes + "\n");
+
+    }
+
+    // {'superclassFacts': ['subClassOf()', ...], 'facts': []}
+
+    public static HashMap<String, ArrayList<String>> obtainPrologFact(Entity xmlEntity) {
+
+        HashMap<String, ArrayList<String>> facts = new HashMap<>();
+        facts.put("entityFacts", new ArrayList<>());
+        facts.put("subclassOfFacts", new ArrayList<>());
 
         for (Entity subClass : xmlEntity.taxonomy) {
 
-            factList.addAll(obtainPrologFact(subClass));
-
+            HashMap<String, ArrayList<String>> taxonomyFacts = obtainPrologFact(subClass);
+            facts.get("entityFacts").addAll(taxonomyFacts.get("entityFacts"));
+            facts.get("subclassOfFacts").addAll(taxonomyFacts.get("subclassOfFacts"));
         }
-
-        return factList;
-    }
-
-    public static String obtainPrologFact(Relationship xmlEntity) {
 
         // wrap name with quotes to respect format of class name (mantain "/", first upper letter, ...)
         String predicateName = "'%s'".formatted(xmlEntity.name);
-
-
-        // extracting references (subject-object) pair
-        ArrayList<String> referenceList = new ArrayList<>();
-
-        for (HashMap<String, String> references : xmlEntity.references) {
-            String subj = references.get("subject");
-            String obj = references.get("object");
-
-            // wrap in quotes class names to preserve formatting
-            referenceList.add("'%s'-'%s'".formatted(subj, obj));
-        }
-
-        String referenceArgument = "[%s]".formatted(String.join(", ", referenceList));
-
 
         // extracting attributes name
         ArrayList<String> attributeList = new ArrayList<>();
 
         for (HashMap<String, Object> attribute : xmlEntity.attributes) {
             attributeList.add((String) attribute.get("name"));
+
         }
 
-        String attributeArguments = "[%s]".formatted(String.join(", ", attributeList));
+        // format of the fact is: PredicateName(AttributesList, ParentsList)
+        String firstArgument = "[%s]".formatted(String.join(", ", attributeList));
 
-        String arguments = "%s, %s".formatted(referenceArgument, attributeArguments);
+        String originalFact = "%s(%s).".formatted(predicateName, firstArgument);
+        facts.get("entityFacts").add(0, originalFact);
 
-        return "%s(%s).".formatted(predicateName, arguments);
+        if (xmlEntity.parentEntity != null) {
+            facts.get("subclassOfFacts").add(0, "subclass_of('%s', '%s').".formatted(xmlEntity.name, xmlEntity.parentEntity.name));
+        }
+
+        return facts;
+    }
+
+    public static HashMap<String, ArrayList<String>> obtainPrologFact(Relationship xmlRelationship) {
+
+        HashMap<String, ArrayList<String>> facts = new HashMap<>();
+        facts.put("relationshipFacts", new ArrayList<>());
+        facts.put("inverseOfFacts", new ArrayList<>());
+
+        // wrap name with quotes to respect format of class name (mantain "/", first upper letter, ...)
+        String predicateName = "'%s'".formatted(xmlRelationship.name);
+
+        // extracting attributes name
+        ArrayList<String> subjObjList = new ArrayList<>();
+
+        for (HashMap<String, String> reference : xmlRelationship.references) {
+            subjObjList.add((String) "'" + reference.get("subject") + "'-'" + reference.get("object") + "'");
+        }
+
+        // extracting attributes name
+        ArrayList<String> attributeList = new ArrayList<>();
+
+        for (HashMap<String, Object> attribute : xmlRelationship.attributes) {
+            attributeList.add((String) attribute.get("name"));
+
+        }
+
+        // format of the fact is: PredicateName(AttributesList, ParentsList)
+        String firstArgument = "[%s]".formatted(String.join(", ", subjObjList));
+        String secondArgument = "[%s]".formatted(String.join(", ", attributeList));
+
+        String originalFact = "%s(%s, %s).".formatted(predicateName, firstArgument, secondArgument);
+        facts.get("relationshipFacts").add(0, originalFact);
+
+        if (xmlRelationship.inverseRelationship != null) {
+            facts.get("inverseOfFacts").add(0, "inverse_of('%s', '%s').".formatted(xmlRelationship.inverseRelationship.name, xmlRelationship.name));
+        }
+
+        return facts;
     }
 
 }
