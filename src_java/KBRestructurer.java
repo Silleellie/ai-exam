fd68b27bc21e85e2007c87942199180583b0303b
Silleellie <antonio.silletti@gmail.com>
2023-05-20 21:53:49 +0200

    created intermediate step for restructuring arc in arc_properties
    Co-authored-by: m-elio <m-elio@users.noreply.github.com>

--- src_java/KBRestructurer.java
+++ src_java/KBRestructurer.java
@@ -74,6 +74,54 @@ public class KBRestructurer {
         return String.join("\n", facts);
     }
 
+    private static void convertArcToArcProperties(String pathOldKB) {
+        try {
+            System.out.println("*************** Restructuring arc in arc properties started ***************");
+            File kbFile = new File(pathOldKB);
+            FileWriter kbWriter = new FileWriter("temp_kb.pl");
+
+            Scanner fileIter = new Scanner(kbFile);
+
+            int processedLines = 0;
+            while (fileIter.hasNextLine()) {
+
+                String line = fileIter.nextLine();
+
+                Pattern pattern = Pattern.compile("arc\\(([0-9]+),\\s*'(.+)',\\s*([0-9]+),\\s*([0-9]+)\\)\\.");
+                Matcher matcher = pattern.matcher(line);
+
+                if (matcher.find()) {
+
+                    String predicateId = matcher.group(1);
+                    String relationshipName = matcher.group(2);
+                    String subjectId = matcher.group(3);
+                    String objectId = matcher.group(4);
+
+                    String arcFact = "arc(%s, %s, %s).".formatted(predicateId, subjectId, objectId);
+                    String arcPropertiesFact = "arc_properties(%s, '{subClass=%s}').".formatted(predicateId, relationshipName);
+
+                    line = "%s\n%s".formatted(arcFact, arcPropertiesFact);
+
+                }
+
+                kbWriter.write(line + "\n");
+                processedLines++;
+
+                if ((processedLines % 100000) == 0) {
+                    System.out.printf("Processed %d lines%n", processedLines);
+                }
+
+            }
+
+            System.out.printf("Arc facts restructured! Now whole file restructuring will start %n%n");
+
+            fileIter.close();
+            kbWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     private static void convertFile(String mode, String pathOldKB, String pathNewKB) {
         try {
@@ -138,6 +186,9 @@ public class KBRestructurer {
         System.out.printf("Please insert conversion mode. Values accepted: %s%n", validValues);
         String mode = input.nextLine();
 
+        System.out.printf("Please choose whether arc predicates should be restructured in arc properties: \"y/n\"%n");
+        String restructureArcs = input.nextLine();
+
 
         if (!(validValues.contains(mode))) {
             throw new IllegalArgumentException(("Value %s is not supported! " +
@@ -146,7 +197,13 @@ public class KBRestructurer {
 
         String outputFilename = "%s%s".formatted(mode, inputFilename);
 
-        convertFile(mode, inputFilename, outputFilename);
+        if (restructureArcs.equals("y")) {
+            convertArcToArcProperties(inputFilename);
+            convertFile(mode, "temp_kb.pl", outputFilename);
+            new File("temp_kb.pl").delete();
+        } else {
+            convertFile(mode, inputFilename, outputFilename);
+        }
 
         String outputPath = FileSystems.getDefault().getPath(outputFilename).toAbsolutePath().toString();
 
