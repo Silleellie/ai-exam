e25a6212fc1dd92aba62404e404ce0db6d7d286b
Silleellie <antonio.silletti@gmail.com>
2023-05-29 15:46:44 +0200

    changed unknown prolog directive
    Co-authored-by: m-elio <m-elio@users.noreply.github.com>

--- src_java/SchemaToProlog.java
+++ src_java/SchemaToProlog.java
@@ -49,7 +49,7 @@ public class SchemaToProlog {
         kbWriter.write(":- use_module(library(lists)).\n");
         kbWriter.write(":- discontiguous inverse_of/2.\n");
         kbWriter.write(":- discontiguous subclass_of/2.\n");
-        kbWriter.write("% Uncomment this if you're using SWI\n% :- set_prolog_flag(unknown, fail).\n\n\n");
+        kbWriter.write(":- unknown(_, fail).\n\n\n");
 
     }
 
